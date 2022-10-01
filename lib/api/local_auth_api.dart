ee6eacd286891ae26e9ee615a6a17c02443376af
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 17:02:16 +0300

    Update local_auth_api.dart

--- lib/api/local_auth_api.dart
+++ lib/api/local_auth_api.dart
@@ -1,13 +1,20 @@
-
+import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 
-class LocalAuthApi{
-  static final _auth=LocalAuthentication();
+class LocalAuthApi {
+  static final _auth = LocalAuthentication();
 
-
-  static Future<bool> authenticate() async{
-    return await _auth.authenticate(localizedReason: 'Kimlik doğrulaması için parmak izini tara');
-
+  static Future<bool> authenticate() async {
+    try {
+      return await _auth.authenticate(
+        localizedReason: 'Kimlik doğrulaması için parmak izini tara',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          useErrorDialogs: true,
+        ),
+      );
+    } on PlatformException catch (e) {
+      return false;
+    }
   }
-
 }
