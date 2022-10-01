334bd2b695948f24a7d3abc6c37be995274da1ee
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 16:22:39 +0300

    Update fingerprint_page.dart

--- lib/page/fingerprint_page.dart
+++ lib/page/fingerprint_page.dart
@@ -1,12 +1,14 @@
+import 'package:fingerprint_auth/main.dart';
 import 'package:flutter/material.dart';
 
 class FingerprintPage extends StatelessWidget {
   const FingerprintPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title:const Text(MyApp.title),
+      centerTitle: true,
+    ),
+  );
 }
