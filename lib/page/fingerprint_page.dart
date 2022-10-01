4cb258558117560143d009fa9a0ba347ccb2a42e
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-01 16:11:09 +0300

    Create fingerprint_page.dart

---
+++ lib/page/fingerprint_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FingerprintPage extends StatelessWidget {
+  const FingerprintPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
